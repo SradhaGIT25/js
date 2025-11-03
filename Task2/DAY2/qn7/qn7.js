78dea679ee6e17c67487cedd9890428ba4bbcb8b
SradhaGIT25 <sradha.s@vonnue.com>
2025-11-03 22:53:17 +0530

    Update qn7.js

--- Task2/DAY2/qn7/qn7.js
+++ Task2/DAY2/qn7/qn7.js
@@ -38,35 +38,6 @@ if(k===1){
 }
 
 function longestWord(sentence){
-    if(typeof sentence !== "string"  || string === null || string === undefined){
-        return false
-    }
-    else{
-        let sentence="I love coding";
-        let longest="";
-        let word=sentence.split(' ');
-        console.log(word);
-
-        for(let i=0;i<word.length-1;i++){
-            if(word[i].length>=word[i+1].length){
-                if(word[i].length>=longest.length){
-                    longest=word[i];
-                }
-            }
-            else{
-                if(word[i].length<word[i+1].length){
-                    if(word[i+1].length>=longest.length){
-                    longest=word[i+1];
-                }
-            }
-        }
-        }
-        return longest;
-            }
-        }
-
-
-function longestWord(sentence){
     let rlong=[];
     if(typeof sentence !== "string"){
         return false
