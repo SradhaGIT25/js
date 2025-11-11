858d8fa6a55d536b439a7708be42d2288557b3f0
sradha <you@example.com>
2025-11-11 08:02:27 +0530

    commits

--- Task 17/qn3.js
+++ Task 17/qn3.js
@@ -1,74 +1,35 @@
 
+    function DeepEqual(a,b){
+    if(typeof a!=="object" || typeof b!=="object"){
+        return false;
+    }
+    else{
+        if(Array.isArray(a)  && Array.isArray(b)){
+            if(a.length!==b.length){
+                return false;
+            }
+        for(let i=0;i<a.length;i++){
+                if(!DeepEqual(a[i],b[i])){
+                        return false;
+                }
+            }
+            return true;
+        }
+    
+    
+        if(Object.keys(a).length!==Object.keys(b).length){
+            return false;
+        }
+            for(let key in a){
+                if(!DeepEqual(a[key],b[key])){
+                return false;
+            }
+        }
+                return true;
+        }
+    }
 
+console.log(DeepEqual({x:1,y:{z:2}},{x:1,y:{z:2}}))
+console.log(DeepEqual([1,2,3],[1,2,3]))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // function DeepEqual(a,b){
-    // if(typeof a!=="object" || typeof b!=="object"){
-    //     return false;
-    // }
-    // else{
-    //     if(Object.entries(a).length===Object.entries(b)){
-    //         for(key in a){
-    //             if(a[key] === b[key]){
-    //             return true;
-    //         }
-    //         else{
-    //             return false;
-    //         }
-    //     }
-
-
-    //     for(let i=0;i<a.length;i++){
-    //             if(a[i]===b[i]){
-    //                     return false;
-    //             }
-    //             else if(Array.isArray(a[i]) || Array.isArray(b[i])){
-    //                 for(let j=0;j<a[i].length;j++){
-    //                     if(a[i][j]!==b[i][j]){
-    //                         return false;
-    //                     }
-    //                 }
-    //                       return  true;
-    //                 }
-    //             }
-    //         }
-    //         else{
-    //             return false;
-    //         }
-
-                
-    //         }
-
-
-    //     }
+        
