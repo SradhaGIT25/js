e11305394d144f4b7c1ad9c7847cedfe659389ae
SradhaGIT25 <sradha.s@vonnue.com>
2025-11-11 08:06:26 +0530

    Update qn6.js

--- Task 17/qn6.js
+++ Task 17/qn6.js
@@ -1,1 +1,28 @@
+function largestPrimeFac(number){
+    let array=[]
+    let arr=[]
+    for(let i=0;i<=number;i++){
+        if(number%i===0){
+            array.push(i);
+        }
+    }
+    console.log(array);
 
+    for(let i=0;i<number;i++){
+        for(let j=0;j<array.length;j++)
+            if(i!==1 || i!==array[j]){
+                if(array[j]%i!==0){
+            arr.push(array[j]);
+        }
+        else{
+            continue;
+        }
+            }
+    }
+    console.log(arr);
+
+  console.log(arr[arr.length-1]);
+
+}
+largestPrimeFac(13195)
+// largestPrimeFac(600851475143)
