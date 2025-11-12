f2b5d767f6a1137fafe5f7b9b83f1ceef5c9f770
SradhaGIT25 <sradha.s@vonnue.com>
2025-11-12 23:44:32 +0530

    Update qn2.ts

--- task19ts/qn2.ts
+++ task19ts/qn2.ts
@@ -1,3 +1,4 @@
+
 function FindAvailableSeats(seatGrid:number[][], groupSize:number){
     let newArray:number[][]=[]
     let array:number[]=[];
@@ -5,18 +6,7 @@ function FindAvailableSeats(seatGrid:number[][], groupSize:number){
     let available:number[][]=[]
     let r:number=seatGrid.length;
     let c:number=seatGrid[0].length;
-// for(let i=0;i<r;i++){
-//     for(let j=0;j<c;j++){
-//         if(seatGrid[i][j]===0){
-//             console.log("empty");
 
-//         }
-//         else if(seatGrid[i][j]===1){
-//             console.log("booked");
-//         }
-
-//         }
-//     }
 
     for(let i=0;i<r;i++){
     for(let j=0;j<c;j++){
@@ -24,44 +14,36 @@ function FindAvailableSeats(seatGrid:number[][], groupSize:number){
             continue;
         }
 
-        // for(let k=0;k<groupSize;k++){
-            if(seatGrid[i][j]==0){
-            // console.log("empty")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+            if(seatGrid[i][j]==0){      
+                array=[];                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
             array.push(i);
             array.push(j);
-            newArray.push(array);
-            array=[];
-            // break;
+            newArray.push([i,j]);
         }
-        // console.log(newArray);
-
-    // }
-
+}
 }
 
-}
-// console.log(newArray);
-
-for(let i=0;i<newArray.length-1;i++){
-    // console.log(newArray[i])
-    // console.log(newArray[i+1])
-    let ele1=newArray[i];
-    let ele2=newArray[i+1];
+for(let i=0;i<r;i++){
+    seating=[]
     for(let j=0;j<newArray.length;j++){
-        if(ele1[j]-ele2[j]===1){
-            seating.push(ele1);
-            seating.push(ele1);
+            let ele1=newArray[j];
+            let ele2=newArray[j+1];
+        if(ele1[0]===ele2[0] && ele1[1]-ele2[1]===1){
+            seating.push(ele1)
+            seating.push(ele2);
         }
-        else{
-            continue;
+        
         }
+        if(seating.length===groupSize){
+            available.push(seating[0]);
+            seating=[]
         }
-        available.push(seating);
     }
 
-    console.log(available[0])
+
+    console.log(available)
     }
     
-// console.log(FindAvailableSeats([[1,0,0],[1,0,1],[1,0,0],[0,0,0],[0,1,0]], 5));
+console.log(FindAvailableSeats([[1,0,0],[1,0,1],[1,0,0],[0,0,0],[0,1,0]], 5));
 // console.log(FindAvailableSeats([[0,0,1],[1,0,1],[1,1,1]], 5));
 // console.log(FindAvailableSeats([[0,0,0],[1,0,1],[1,1,1]], 5));
