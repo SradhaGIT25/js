c77b107b178ac875e014db0baed70bb6599c28a8
sradha <you@example.com>
2025-11-20 19:04:28 +0530

    commits

---
+++ task20nov/qn1/qn1.js
@@ -0,0 +1,57 @@
+const add=document.getElementById("add");
+const mark=document.getElementById("mark");
+const scrollContainer=document.getElementById("scroll-Container");
+
+
+let p=0;
+async function fetchData(){
+    p++;
+    const response=await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${p}&_limit=10`);
+    const data=await response.json();
+    console.log(data);
+
+    for(let i=0;i<data.length;i++){
+       const element=`<div class="one"><p>UserId : ${data[i].userId}</p><p>Id : ${data[i].id}</p><p>Title : ${data[i].title}</p><p>Body : ${data[i].body}</p></div>`
+       add.insertAdjacentHTML("beforeend",element);
+    }
+}
+fetchData()
+document.addEventListener("scrollend",()=>{
+mark.style.width=`100%`
+fetchData()
+})
+
+
+// window.addEventListener("scroll",(e)=>{
+
+
+
+//     const add=document.getElementById("add");
+// const mark=document.getElementById("mark");
+// const scrollContainer=document.getElementById("scroll-Container");
+
+
+// let p=0;
+// async function fetchData(){
+//     p++;
+//     const response=await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${p}&_limit=10`);
+//     const data=await response.json();
+//     // console.log(data);
+
+//     for(let i=0;i<data.length;i++){
+//        const element=`<div class="one"><p>UserId : ${data[i].userId}</p><p>Id : ${data[i].id}</p><p>Title : ${data[i].title}</p><p>Body : ${data[i].body}</p></div>`
+//        add.insertAdjacentHTML("beforeend",element);
+//     }
+// }
+// fetchData()
+// // 
+// console.log(add[0]);
+// add.addEventListener("scroll",()=>{
+//     console.log(add.clientHeight)
+// if(add.scrollTop===add.scrollHeight){
+// mark.style.width=`100%`;
+// fetchData();
+// }
+// })
+
+// })
