f040b1ebea50386c8994c7c390d7dbb374677599
sradha <you@example.com>
2025-11-29 13:09:42 +0530

    commits

---
+++ Test29nov/qn2.ts
@@ -0,0 +1,7 @@
+function expired(ms){
+    let sec=Math.floor(ms/1000)%60;
+    let mins=Math.floor(sec/1000*60)%60
+    let hours=Math.floor(sec/1000*60*60)%24
+    let days=Math.floor(sec/1000*60*60*24)
+    
+}
